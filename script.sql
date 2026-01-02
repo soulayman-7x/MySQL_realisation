8a6342f058b7d0209d82d0d15394dab524982a11
soulayman-7x <idbahasevenx@gmail.com>
2026-01-02 08:29:46 +0000

    Update SQL script >> ADD post image Table

--- script.sql
+++ script.sql
@@ -1,22 +1,30 @@
 create database if not exists blogDB character set utf8mb4 collate utf8mb4_general_ci;
 use blogDB;
 
+-- =================
+-- 1. Table: User
+-- ================
 create table if not exists user (
-	user_id INT auto_increment primary key,
+    user_id INT auto_increment primary key,
     user_name varchar(45) not null,
-	user_email varchar(200) unique not null,
+    user_email varchar(200) unique not null,
     user_password varchar(255) not null,
-    INDEX index_name (user_name),
-    INDEX index_email (user_email)
-) comment = 'Table to store blog users';
+    INDEX index_name (user_name)
+) comment 'Table to store blog users';
 
+-- ===================
+-- 2. Table: Category
+-- ===================
 create table if not exists category (
-	category_id INT auto_increment primary key,
+    category_id INT auto_increment primary key,
     category_name varchar(45) not null
-) comment = 'Table to store article categories';
+) comment 'Table to store article categories';
 
+-- ==================
+-- 3. Table: Article
+-- ==================
 create table if not exists article (
-	article_id INT auto_increment primary key,
+    article_id INT auto_increment primary key,
     article_title varchar(100) not null,
     article_content text not null,
     article_date_pub datetime default current_timestamp,
@@ -26,25 +34,31 @@ create table if not exists article (
     foreign key (user_id) references user(user_id) on delete cascade,
     foreign key (category_id) references category(category_id) on delete set null,
     INDEX index_article (article_title)
-) comment = 'Table to store blog articles';
+) comment 'Table to store blog articles';
 
+-- ==================
+-- 4. Table: Comment
+-- =================
 create table if not exists comment (
-	comment_id INT auto_increment primary key,
+    comment_id INT auto_increment primary key,
     comment_content text not null,
     comment_pub_date datetime default current_timestamp,
     user_id INT,
     article_id INT,
-    foreign key (user_id) references user(user_id) on delete cascade -- if user is deleted, delete their comments
-    foreign key (article_id) references article(article_id) on delete cascade -- if article is deleted, delete its comments
-) comment = 'Table to store comments on articles';
+    foreign key (user_id) references user(user_id) on delete cascade,
+    foreign key (article_id) references article(article_id) on delete cascade
+) comment 'Table to store comments on articles';
 
+-- =====================
+-- 5. Table: Post Image
+-- =====================
 create table if not exists post_image (
-	image_id INT auto_increment primary key,
-    image_path 	varchar(255) not null -- Path to the image file,
+    image_id INT auto_increment primary key,
+    image_path  varchar(255) not null, 
     article_id INT not null,
-	is_featured tinyint default 0,
+    is_featured tinyint default 0,
     foreign key (article_id) references article(article_id) on delete cascade on update cascade
-) comment = 'Table to store images of articles';
+) comment 'Table to store images of articles';
 
 
 -- ==========================================
@@ -74,8 +88,6 @@ insert into comment (comment_content, user_id, article_id) values
 ('I love SQL too!', 3, 1);
 
 INSERT INTO post_image (image_path, article_id, is_featured) VALUES 
-('uploads/sql_intro.jpg', 1, 1),   -- Featured image (cover) for the first article
-('uploads/python_basics.png', 2, 1),    -- Featured image (cover) for the second article
-('uploads/sql_diagram.jpg', 1, 0); -- A regular image within the first article
-
-
+('uploads/sql_intro.jpg', 1, 1),   
+('uploads/python_basics.png', 2, 1),    
+('uploads/sql_diagram.jpg', 1, 0);
